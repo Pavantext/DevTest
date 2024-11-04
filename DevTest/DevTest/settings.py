52bf344765f6c2943ebf9c78a80db476e9cf30d7
Pavantext <pkabcd217@gmail.com>
2024-11-04 17:05:13 +0530

    "Updated Details"

--- DevTest/DevTest/settings.py
+++ DevTest/DevTest/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v#u2%*z=gk^bq2r2b*^qfzxy)3gr-_&+a2d%0m$1x&x*j-81-w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
